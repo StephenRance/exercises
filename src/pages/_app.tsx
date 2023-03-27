3316e006396a6848415fa403bbccb617e1d42ed6
Stephen Rance <stephen.john.rance@gmail.com>
2023-03-27 19:26:36 +0100

    style: add base styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,6 @@
+import '@/styles/fonts.css';
+import '@/styles/global.css';
+import '@/styles/variables.css';
 import type { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps) => (
